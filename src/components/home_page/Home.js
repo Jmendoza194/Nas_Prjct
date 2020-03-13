343933fd476adf94203121765f304c8352784c34
Jose Mendoza <joseemendoza194@gmail.com>
2020-03-13 14:42:39 -0700

    Added more info

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -20,18 +20,18 @@ class Home extends React.Component{
             iconPics:['https://cdn1.iconfinder.com/data/icons/education-set-4/512/information-512.png',
                       'https://cdn4.iconfinder.com/data/icons/web-interface-6/12/player-circle-512.png',
                       'https://cdn4.iconfinder.com/data/icons/vectory-symbols/40/help_1-512.png'],
-            vidName:['We Are Mauna Kea', 'Kū Haʻaheo','No Video', 'No Video'],
+            vidName:['We Are Mauna Kea', 'Kū Haʻaheo','The Mapuche of Chile', '500 Year Conflict'],
             vidDescription:['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
-            videos:['https://www.youtube.com/embed/A2w8TWpSfgQ','https://www.youtube.com/embed/WbMX_LoT_YA','',''],
+            videos:['https://www.youtube.com/embed/A2w8TWpSfgQ','https://www.youtube.com/embed/WbMX_LoT_YA','https://www.youtube.com/embed/41pnfq27Oak','https://www.youtube.com/embed/sJJ_9UEX86k'],
             MaunaKeaName:['Mauna Kea Legal Defense Fund','Mauna Kea Protection Fund','Mauna Kea ‘Ohana Fund'],
-            MapucheName:['?a','b','c'],
+            MapucheName:['Mapuche Foundation','b','c'],
             MaunaKeaLinks:['https://org.salsalabs.com/o/2699/p/salsa/donation/common/public/?donate_page_KEY=7145',
                             'https://7genfund.abilafundraisingonline.com/hpmaunakea',
                             'https://www.protectmaunakea.net/mauna-kea-ohana-fund'],
-            MapucheLinks:['a','b','c']
+            MapucheLinks:['https://www.mapuche.nl/','b','c']
             
         }
     }
--- src/components/home_page/Videos/VideoBlock.js
+++ src/components/home_page/Videos/VideoBlock.js
@@ -6,7 +6,7 @@ const VideoBlock = ({ video, videoName, videoDescription }) =>{
         <div className='video-item'>
             <div>
                 <h3>{videoName}</h3>
-                <p className='small-margin-up'>{videoDescription}</p>
+                <p className='small-margin-up'></p>
                 <iframe className='small-margin-up' width="560" height="315" src={video} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
             </div>
        </div>
